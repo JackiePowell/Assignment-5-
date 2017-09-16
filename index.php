8e9fa1a4dd0ee7933b8466d024a2aed4f6ac0aa2
JDDenzer9 <31746765+JDDenzer9@users.noreply.github.com>
2017-09-16 17:37:53 -0400

    index
    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Assignment 4</title>
+	</head>
+	
+	<body>
+	
+	<header>
+		<h1>Welcome to our website</h1>
+		<h2>Created by John, Michelle and the other girl</h2>
+	</header>
+	
+	</body>
