02dd88b9663cabfe27a8afe631893f317186e069
JDDenzer9 <31746765+JDDenzer9@users.noreply.github.com>
2017-09-16 17:32:11 -0400

    initial
    Assignment 4

---
+++ johndenzer.php
@@ -0,0 +1,45 @@
+<!-- 
+John Denzer
+Assignment 4 
+9/16/17
+-->
+
+<!doctype html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>John Denzer</title>
+	</head>
+
+	<body>
+	
+		<header>
+			<h1>John Denzer</h1>
+			<h2>An Introduction</h2>
+		</header>
+		
+		<h3>Interesting Facts</h3>
+		<p>- Born in San Francisco <br />
+		   - Played baseball for 15 years <br />
+		   - Studying Financial Economics <br />
+		   - Interested in a career in investment banking</p>
+		
+		<h3>My Experience</h3>
+		<p>- Worked at Exit Partners, LLC.  Worked on CIMs, modeling and general day to day analyst assignments where I learned a lot about working in a group in an Investment Banking environment. <br />
+		   - Created own computer building business called Jack's PCs in high school focused on buying computer parts, building the computer, and installing in home of the clients. <br />
+		   - Worked as the president of the Young Men's Service League in high school helping out with philanthropies dedicated to special needs children around the Dallas Metroplex. </p>
+		
+		<h3>My Schoolwork</h3>
+		<p>I have taken classes primarily in the Economics, Finance and Computer Science worlds.  Some of the Economics classes include ECO 108, 207, 209, 230, 231w and I am currently a Teaching Assistant for ECO 207.  I have taken Accounting 201 and am currently taking Finance 205.  In the Computer Science world, I have taken CSC 170, 171, 172 and now 174.  I am looking to leverage my classwork in Computer Science to make myself look like a more applicable candidate for investment banking roles. My favorite class that I have taken is ECO 207 as I undersood it and really enjoyed the professor's lectures. </p>
+		
+		<h3>My Baseball Career</h3>
+		<p>I have played baseball now for 15 years or so and it was my lifelong dream to play college baseball and luckily I was given that opportunity when I came to the University of Rochester.  I am a right handed starting pitcher and I actually set a school record this last season for least amount of walks per nine innings pitched.  I also was 2nd in the NCAA for the same stat this last season. I love baseball and I hope the last two seasons of my career do not go too quickly.</p>
+		
+		<h3>My Interests</h3>
+		<p>I am very interested in sports and in computers.  I follow European soccer very closely and I am an avid computer builder.  I am a huge Tottenham Hotspur fan and follow the news religiously regarding what is going on with them currently.  I enjoy following tech news as well and one of my favorite companies is AMD and I love following how their stock moves so quickly in the market due to their contributions to the semiconductor space. </p>		
+
+	</body>
+	
+</html>
---
+++ michellebushoy.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Michelle Bushoy</title>
+</head>
+
+<body>
+		<header>
+			<h1> Michelle Bushoy </h1>
+			<div> Welcome to my website </div>
+		</header>
+
+		<h2>About Me</h2>
+		<p>My name is Michelle, and I am a junior at University of Rochester. I am from the greater Boston area, and live in a suburb called Newton. I have an older sister named Yael. She is currently living in Manhattan and works at J.P. Morgan. I am studying Digital Media Studies with a minor in Computer Science. My scope of interest within my major lies in the intersection of code and design. I work best visually, and love seeing my designs come to life through code. Aside from my educational interests, I enjoy biking, traveling and walking my Irish Setter. My dog's name is Archie. </p>
+
+		<h2> Education </h2>
+		<h3>University of Rochester</h3>
+			<ul>
+  				<li>Major: Digital Media Studies, B.A. </li>
+  				<li>Minor: Computer Science</li>
+  				<li>Recipient of Dean's Merit Scholarship</li>
+  				<li>Sinai Scholar</li>
+			</ul>
+
+		<h3> Newton South High School </h3>
+			<ul>
+  				<li>Newton South High School Senate</li>
+  				<li>Big Brother, Big Sister </li>
+  				<li>Children of Ghana</li>
+			</ul>
+
+		<h2>Courses </h2>			
+			<ul>
+  				<li>CSC 170 </li>
+  				<li>MTH 161</li>
+  				<li>MTH 162</li>
+  				<li>CSC 171</li>
+  				<li>MTH 159</li>
+  				<li>DMS 103</li>
+  				<li>ECO 108</li>
+  				<li>HIS 285</li>
+  				<li>CSC 131</li>
+			</ul>
+
+</body>
+
+</html>
