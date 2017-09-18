380b467c8ffd960f5c10c1fe7e9f251029ef43c8
mbushoy27 <32023622+mbushoy27@users.noreply.github.com>
2017-09-18 13:07:00 -0400

    Update daniellabloom.php

--- daniellabloom.php
+++ daniellabloom.php
@@ -5,6 +5,9 @@
 <head>
 	<meta charset="utf-8">
 	<title>Information about Daniella Bloom</title>
+	<link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="navigation.css">
 </head>
 
 <body>
