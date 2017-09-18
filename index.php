4a3e47f1c8c7723ee3d52fc267e454de05880b19
JDDenzer9 <31746765+JDDenzer9@users.noreply.github.com>
2017-09-18 11:26:48 -0400

    final IA work

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 	<body>
 	
 		<header>
-			<h1>Welcome to our website</h1>
-			<h2>Created by John, Michelle and the other girl</h2>
+			<h1>Welcome to Our Assignment 4 Website</h1>
+			<h2>Created by Michelle, Daniella, and John</h2>
 		</header>
 		
 		<nav class="menu">
@@ -33,15 +33,15 @@
 		<h3>John Denzer</h3>
 		<p>John was born in Redwood City, CA on December 14th, 1996.  John is a member of the varsity baseball team at the University of Rochester.  He is a starting pitcher on the team.  John is looking to hopefully work on Wall Street after college and is now in the process of searching for a summer internship in the field.  John enjoys building computers and watching European soccer in his free time. </p>
 		
-		<p>To learn more about John, visit his <a href="johndenzer.php"> page </a></p>
+		<p>To learn more about John, visit his <a href="johndenzer.php"> page here </a></p>
 		
 		<h3>Michelle Bushoy</h3>
 		<p>Michelle is a junior at University of Rochester. She is from Boston and is currently residing in Newton. She is studying Digital Media Studies with a minor in Computer Science. She is interested in the intersection of code and design. Aside from her educational interests, she enjoys biking, traveling and walking her dog, Archie. </p>
 		
-		<p>To learn more about Michelle, visit her <a href="michellebushoy.php"> page </a></p>
+		<p>To learn more about Michelle, visit her <a href="michellebushoy.php"> page here </a></p>
 		
 		<h3>Daniella Bloom</h3>
 		<p>Daniella was born and grew up in New York City on the Upper West Side. She lived with my mom, dad, brother Jamie and brother Paul. She started doing gymnastics recreationally and when she was 8, she joined a team. She stopped doing gymnastics right before highschool started.</p>
 		
-		<p>To learn more about Daniella, visit her <a href="daniellabloom.php"> page </a></p>
+		<p>To learn more about Daniella, visit her <a href="daniellabloom.php"> page here </a></p>
 	</body>
