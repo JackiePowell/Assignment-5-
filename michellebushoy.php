a70a1cb16e1c100ccac3c41e89a95807abcbfe5d
mbushoy27 <32023622+mbushoy27@users.noreply.github.com>
2017-09-18 13:08:02 -0400

    Update michellebushoy.php

--- michellebushoy.php
+++ michellebushoy.php
@@ -5,6 +5,9 @@
 <head>
 <meta charset="UTF-8">
 <title>Michelle Bushoy</title>
+<link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="navigation.css">
 </head>
 
 <body>
