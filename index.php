d7f08dd598ece533285d94bd1b34a72bbc377518
backerm3 <backerm3@u.rochester.edu>
2017-10-01 23:12:45 -0400

    Remove Greg, add Michelle on index.php

--- index.php
+++ index.php
@@ -17,21 +17,9 @@
             <h1>About Us</h1>
             <div class="minibios">
                 <div class="bio para2 shad">
-                    <h2>Greg Marshall</h2>
-                    <p>"Greg Marshall (often referred to as Greg The Great for
-                      his genius and winning personality) was born in Syracuse, New York. He
-                      lived out the majority of his childhood there before graduating from
-                      high school at Jordan-Elbridge High School. He decided he would attend
-                      the University of Rochester to pursue a degree in Biology, a science
-                      which he maintains a love for today, but found that it was not a field
-                      he wished to develop a career in. Pivoting substantially, he entered a
-                      business program at Rochester's Simon School of Business. It was also
-                      around this time he chose to tryout for the Men's rowing team.
-                      It represented a transition from years of running, so the significant
-                      cardiovascular nature of rowing appealled to him.
-                      He continues his pursuit of a B.S. in business today, and his love for
-                      his team and the sport of rowing grows with every early morning practice."</p>
-                    <a href= "gregmarshall.php">Click for more about Greg</a>
+                    <h2>Michelle Bushoy</h2>
+                    <p>"Some content should go here."</p>
+                    <a href= "michellebushoy.php">Click for more about Michelle</a>
                 </div>
 
                 <div class="bio para2 shad">
