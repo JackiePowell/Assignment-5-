10bb548062aa2cf2e89a06da7f07575067c71ceb
backerm3 <backerm3@u.rochester.edu>
2017-10-01 23:34:40 -0400

    Adjust page structure to match other pages
    Or at least get as close as possible...

--- michellebushoy.php
+++ michellebushoy.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 
 <html>
-
 	<head>
-	<meta charset="UTF-8">
-	<title>Michelle Bushoy</title>
+		<meta charset="UTF-8">
+		<title>Michelle Bushoy</title>
 	</head>
 
 	<body>
@@ -13,43 +12,40 @@
 		include "header.inc.php";
 		?>
 		
-		<h1> Michelle Bushoy </h1>
-		<h2> An Introduction</h2>
+		<h1>Michelle Bushoy</h1>
+		<em>An Introduction</em>
 		
-
-		<h3>About Me</h3>
 		<p>My name is Michelle, and I am a junior at University of Rochester. I am from the greater Boston area, and live in a suburb called Newton. I have an older sister named Yael. She is currently living in Manhattan and works at J.P. Morgan. I am studying Digital Media Studies with a minor in Computer Science. My scope of interest within my major lies in the intersection of code and design. I work best visually, and love seeing my designs come to life through code. Aside from my educational interests, I enjoy biking, traveling and walking my Irish Setter. My dog's name is Archie. </p>
-
-		<h3> Education </h3>
-		<h4>University of Rochester</h4>
+		
+		<div> <!-- We'll add the proper class here later -->
+			<h2>Education</h2>
+			<h3>University of Rochester</h3>
 			<ul>
-  				<li>Major: Digital Media Studies, B.A. </li>
-  				<li>Minor: Computer Science</li>
-  				<li>Recipient of Dean's Merit Scholarship</li>
-  				<li>Sinai Scholar</li>
-			</ul>
-
-		<h4> Newton South High School </h4>
-			<ul>
-  				<li>Newton South High School Senate</li>
-  				<li>Big Brother, Big Sister </li>
-  				<li>Children of Ghana</li>
-			</ul>
-
-		<h3>Courses </h3>			
-			<ul>
-  				<li>CSC 170 </li>
-  				<li>MTH 161</li>
-  				<li>MTH 162</li>
-  				<li>CSC 171</li>
-  				<li>MTH 159</li>
-  				<li>DMS 103</li>
-  				<li>ECO 108</li>
-  				<li>HIS 285</li>
-  				<li>CSC 131</li>
+				<li>Major: Digital Media Studies, B.A. </li>
+				<li>Minor: Computer Science</li>
+				<li>Recipient of Dean's Merit Scholarship</li>
+				<li>Sinai Scholar</li>
 			</ul>
 			
+			<h3>Courses</h3>			
+			<ul>
+				<li>CSC 170</li>
+				<li>MTH 161</li>
+				<li>MTH 162</li>
+				<li>CSC 171</li>
+				<li>MTH 159</li>
+				<li>DMS 103</li>
+				<li>ECO 108</li>
+				<li>HIS 285</li>
+				<li>CSC 131</li>
+			</ul>
 
+			<h3>Newton South High School</h3>
+			<ul>
+				<li>Newton South High School Senate</li>
+				<li>Big Brother, Big Sister </li>
+				<li>Children of Ghana</li>
+			</ul>
+		</div>
 	</body>
-
 </html>
