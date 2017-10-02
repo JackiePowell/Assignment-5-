70808394adea85eaa021118fa7bfc2afeb47529d
backerm3 <backerm3@u.rochester.edu>
2017-10-01 23:14:00 -0400

    Remove Greg, add Michelle on header include

--- header.inc.php
+++ header.inc.php
@@ -3,7 +3,7 @@
         <nav class="main-menu">
             <ul>
               <li<?php if ($page == "home") echo " class=\"current\"" ?>><a href="index.php">Home</a></li>
-              <li<?php if ($page == "greg") echo " class=\"current\"" ?>><a href="gregmarshall.php">Greg Marshall</a></li>
+              <li<?php if ($page == "michelle") echo " class=\"current\"" ?>><a href="michellebushoy.php">Michelle Bushoy</a></li>
               <li<?php if ($page == "ben") echo " class=\"current\"" ?>><a href="benackerman.php">Ben Ackerman</a></li>
               <li<?php if ($page == "jackie") echo " class=\"current\"" ?>><a href="jackiepowell.php">Jackie Powell</a></li>
             </ul>
