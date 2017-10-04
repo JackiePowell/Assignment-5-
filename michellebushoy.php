6a4ed30f940d3e6659e24c0dcd2967622648ce3a
mbushoy27 <32023622+mbushoy27@users.noreply.github.com>
2017-10-03 20:38:36 -0400

    Update michellebushoy.php

--- michellebushoy.php
+++ michellebushoy.php
@@ -3,10 +3,10 @@
 
 <head>
 	<meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="css/app.css">
 	<title>Michelle Bushoy</title>
 </head>
 
@@ -20,12 +20,12 @@
 	</header>
 		
 
-		<section class="page" style="padding: 40px"> <!-- section for the whole page minus nav, 40px padding on each side -->
-		<div class="grid-x">
-		<div class="large-12 cell" >
-		<h1>Michelle Bushoy</h1>
-		<em>Dog lover, aspiring designer, and more. </em>
-		<p>Michelle Bushoy is a Digital Media Studies major and Computer Science minor at the 
+	<section class="page" style="padding: 40px"> <!-- section for the whole page minus nav, 40px padding on each side -->
+	<div class="grid-x">
+	<div class="large-12 cell" >
+	<h1>Michelle Bushoy</h1>
+	<em>Dog lover, aspiring designer, and more. </em>
+	<p>Michelle Bushoy is a Digital Media Studies major and Computer Science minor at the 
             University of Rochester. Michelle hopes to continue her studies in digital media to 
             eventually work in web design, search engine optimization, or user experience. This 
             past summer, she interned at Salary.com as a digital marketing intern. There, she mostly 
@@ -36,29 +36,29 @@
         </div>
 		
         <section class="projects" style="padding-top:40px"> <!-- section just for the projects, 40px padding on top -->
-		<h2>Projects:</h2>
+	<h2>Projects:</h2>
 
-		<div class="grid-x">
-		<div class="small-3 large-4 cell" style="padding-right: 20px">
-		<h3>Gronk Fitness Products: Current SEO Analyst Intern</h3>
-		<p>Created and implemented a Search Engine Optimization (SEO) plan for Gronk Fitness Products shopify website. </p>
-		</div>
+	<div class="grid-x">
+	<div class="small-3 large-4 cell" style="padding-right: 20px">
+	<h3>Gronk Fitness Products: Current SEO Analyst Intern</h3>
+	<p>Created and implemented a Search Engine Optimization (SEO) plan for Gronk Fitness Products shopify website. </p>
+	</div>
 
-		<div class="small-3 large-4 cell" style="padding-right: 20px" >
-		<h3>Salary.Com: Summer Intern</h3>
-		<p>Worked on a team of digital marketers to create and implement a Search Engine Optimization (SEO) plan. Worked primarily on keyword expansion. </p>
-		</div>
+	<div class="small-3 large-4 cell" style="padding-right: 20px" >
+	<h3>Salary.Com: Summer Intern</h3>
+	<p>Worked on a team of digital marketers to create and implement a Search Engine Optimization (SEO) plan. Worked primarily on keyword expansion. </p>
+	</div>
 
-		<div class="small-3 large-4 cell" >
-		<h3>AB Data Consultants: Summer Intern</h3>
-		<p>Designed wireframes for the landing page of a employee compensation management system web portal. Used HTML, CSS and jQuery to create a mock landing page for the portal. </p>
-		</div>
+	<div class="small-3 large-4 cell" >
+	<h3>AB Data Consultants: Summer Intern</h3>
+	<p>Designed wireframes for the landing page of a employee compensation management system web portal. Used HTML, CSS and jQuery to create a mock landing page for the portal. </p>
+	</div>
 
-		</div>
+	</div>
 		
-		</section> <!-- end project section -->
+	</section> <!-- end project section -->
 
-		</section> <!-- end page section -->
+	</section> <!-- end page section -->
 
 <script src="js/vendor/jquery.js"></script>
 <script src="js/vendor/what-input.js"></script>
