0cd41aff98a93fbfd413adc0b7b0a8f4d0877bc1
backerm3 <backerm3@u.rochester.edu>
2017-10-05 16:14:21 -0400

    Add includes to benackerman.php

--- benackerman.php
+++ benackerman.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/foundation.css">
-	<link rel="stylesheet" href="css/app.css">
-	<title>Ben Ackerman</title>
-</head>
-
-<body>
-
-	<header>
-		<?php
-		$page = "ben";
-		include "header.inc.php";
-		?>
-	</header>
+<?php
+$page = "ben";
+include "master_header.inc.php";
+?>
 	<img src="images/ben.png" alt="ben">
 
 
@@ -72,11 +56,6 @@
 
 
 
-  <script src="js/vendor/jquery.js"></script>
-  <script src="js/vendor/what-input.js"></script>
-  <script src="js/vendor/foundation.js"></script>
-  <script src="js/app.js"></script>
-		
-</body>
-
-</html>
+<?php
+include "master_footer.inc.php";
+?>
